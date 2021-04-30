effc846ce3453754478c79f2ddf9a9ff58144cc7
Grant Taggart <granttaggartsf@gmail.com>
2021-04-30 10:22:04 -0700

    Create basic CRUD routes

--- server/app.js
+++ server/app.js
@@ -13,12 +13,40 @@ app.get('/:id', (req, res) => {
   res.sendFile(path.resolve('public/index.html'));
 });
 
+// app.get('/api/sizes/:id', (req, res) => {
+//   db.getProductSizeAsync(req.params.id)
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => console.error(err));
+// });
+
+//Create
+
+app.post('/api/sizes/:id', (req, res) => {
+  console.log('req', req, 'res', res);
+});
+
+//Read
+
 app.get('/api/sizes/:id', (req, res) => {
-  db.getProductSizeAsync(req.params.id)
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => console.error(err));
+  db.getProductSize(req.params.id, (data) => {
+    res.send(() => {
+      console.log('data', data);
+    });
+  });
+});
+
+//Update
+
+app.put('/api/sizes/:id', (req, res) => {
+  console.log(req.params.id);
+});
+
+//Delete
+
+app.delete('/api/sizes/:id', (req, res) => {
+  console.log(req.params.id);
 });
 
 module.exports = app;
