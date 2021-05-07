75a83041439cd0288283661e8f573c08993849bc
Grant Taggart <granttaggartsf@gmail.com>
2021-05-07 11:14:19 -0700

    Complete seed script

--- server/database/seed.js
+++ server/database/seed.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('./database.js');
 const models = require('./databaseModels.js');
+const productSizes = models.productSizes;
 const generateSeedData = require('./generateSeedData.js');
 const dbname = process.env.NODE_ENV === 'test' ? 'ikea-test' : 'ikea';
 const host = process.env.DB_HOST_ENV === 'docker' ? 'mongo:27017' : 'localhost';
@@ -10,16 +11,31 @@ const host = process.env.DB_HOST_ENV === 'docker' ? 'mongo:27017' : 'localhost';
 
 const seedDBAsync = async (callback) => {
   console.log(`Connecting to DB mongodb://${host}/${dbname}`)
-  await mongoose.connect(`mongodb://${host}/${dbname}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
-  models.productSizes.deleteMany();
-  const data = await generateSeedData();
-  db.setProductSizesAsync(data)
-    .then((results) => {
-      // console.log(results);
-    })
-    .catch(err => console.error(err))
-    .then(() => mongoose.disconnect());
+  await mongoose.connect(`mongodb://${host}/${dbname}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .then(async () => {
+    let entries = [];
+    for (let i = 1; i <= 10000000; i++) {
+      var data = generateSeedData(i);
+      entries.push(data);
+      if (entries.length === 100000) {
+        await productSizes.insertMany(entries);
+        console.log(`${i} entries saved`);
+        entries = [];
+      }
+    }
+  })
+  .catch(err => console.error(err))
+  .then(() => mongoose.disconnect());
 }
+//   models.productSizes.deleteMany();
+//   const data = await generateSeedData();
+//   db.setProductSizesAsync(data)
+//     .then((results) => {
+//       // console.log(results);
+//     })
+//     .catch(err => console.error(err))
+//     .then(() => mongoose.disconnect());
+// }
 
 seedDBAsync(err => {
   if (err) {
@@ -27,4 +43,4 @@ seedDBAsync(err => {
   } else {
     console.log('Database has been seeded!');
   }
-});
+})
