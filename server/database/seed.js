1ef5b6031422e4ff7241f6955042827e6a637f40
Grant Taggart <granttaggartsf@gmail.com>
2021-05-11 11:20:04 -0700

    Complete seeding script 7:22 for whole process

--- server/database/seed.js
+++ server/database/seed.js
@@ -5,28 +5,51 @@ const productSizes = models.productSizes;
 const generateSeedData = require('./generateSeedData.js');
 const dbname = process.env.NODE_ENV === 'test' ? 'ikea-test' : 'ikea';
 const host = process.env.DB_HOST_ENV === 'docker' ? 'mongo:27017' : 'localhost';
+const fs = require('fs');
+const csvWriter = require('csv-write-stream');
+var writer = csvWriter();
+
+const dataGen = () => {
+  fs.writeFileSync('server/database/__data__/product-size.json', '[');
+  let entries = [];
+  for (let i = 1; i <= 10000000; i++) {
+    var data = JSON.stringify(generateSeedData(i), null, 2);
+    entries.push(data);
+    if (entries.length === 10000) {
+      fs.appendFileSync('server/database/__data__/product-size.json', entries);
+      console.log(`${i} entries saved`);
+      entries = [];
+      if (i !== 10000000) {
+        fs.appendFileSync('server/database/__data__/product-size.json', ',');
+      }
+    }
+  }
+  fs.appendFileSync('server/database/__data__/product-size.json', ']');
+};
+
+dataGen();
 
 
 /* Data Insertion */
 
-const seedDBAsync = async (callback) => {
-  console.log(`Connecting to DB mongodb://${host}/${dbname}`)
-  await mongoose.connect(`mongodb://${host}/${dbname}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-  .then(async () => {
-    let entries = [];
-    for (let i = 1; i <= 10000000; i++) {
-      var data = generateSeedData(i);
-      entries.push(data);
-      if (entries.length === 100000) {
-        await productSizes.insertMany(entries);
-        console.log(`${i} entries saved`);
-        entries = [];
-      }
-    }
-  })
-  .catch(err => console.error(err))
-  .then(() => mongoose.disconnect());
-}
+// const seedDBAsync = async (callback) => {
+//   console.log(`Connecting to DB mongodb://${host}/${dbname}`)
+//   await mongoose.connect(`mongodb://${host}/${dbname}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+//   .then(async () => {
+//     let entries = [];
+//     for (let i = 1; i <= 10000000; i++) {
+//       var data = generateSeedData(i);
+//       entries.push(data);
+//       if (entries.length === 100000) {
+//         await productSizes.insertMany(entries);
+//         console.log(`${i} entries saved`);
+//         entries = [];
+//       }
+//     }
+//   })
+//   .catch(err => console.error(err))
+//   .then(() => mongoose.disconnect());
+// }
 //   models.productSizes.deleteMany();
 //   const data = await generateSeedData();
 //   db.setProductSizesAsync(data)
@@ -37,10 +60,10 @@ const seedDBAsync = async (callback) => {
 //     .then(() => mongoose.disconnect());
 // }
 
-seedDBAsync(err => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Database has been seeded!');
-  }
-})
+// seedDBAsync(err => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('Database has been seeded!');
+//   }
+// })
